578b918e45006d174c360f5ef0c1d63b1f2bab14
himalayachaurasia <himalya.bncet@gmail.com>
2020-11-02 15:43:25 +0530

    changes

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
 	
 	
 }
-/* style end css */
+/* style end */
 </style>
 </head>
 <body class="disable-select"oncopy="return false" oncut="return false" onpaste="return false" onselect="return false">
